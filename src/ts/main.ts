1da298865af72e9cab8564fc658d9a90e2dea8f1
MickeCardaba <mikael.cardaba@gmail.com>
2023-12-11 16:12:20 +0100

    :sparkles: big drink display

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,26 @@
-import './..scss/style.scss'
+import './../scss/style.scss'
+
+
+
+const lastSection = document.getElementById("lastsection")
+const imgUrl = "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg";    //strDrinkThumb[i]
+const image = document.createElement("img");
+const drinkNameHeading = document.createElement("h3");
+const licourHeading = document.createElement("h4");
+const drinkInstructions = document.createElement("p");
+
+
+image.id = "bigImage";
+
+image.src = imgUrl;    
+image.alt = "Margarita";    //strDrink[i]
+
+drinkNameHeading.innerHTML = "Margarita";    //strDrink[i]
+licourHeading.innerHTML = "Gin";   //ingridient[i]
+drinkInstructions.innerHTML = "Lorem ipsum dolor sit amet, ut nibh splendide qui, qui brute civibus id. Per no erant oporteat adolescens, ei liber graecis noluisse mel. Nam nulla movet imperdiet id, modus quaestio te vis. Sed ei laudem suscipit partiendo, quis justo philosophia ut pri, sit te oblique philosophia concludaturque.";
+
+lastSection?.appendChild(image);
+lastSection?.appendChild(drinkNameHeading);
+lastSection?.appendChild(licourHeading);
+lastSection?.appendChild(drinkInstructions);
+
